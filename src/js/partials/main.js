2bd8448828c5046b4d6059db32a13407fbdf22b6
PipZip <37911683+PipZip@users.noreply.github.com>
2021-06-03 20:11:45 +0600

    temp

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -35,14 +35,68 @@
 		},
 	});
 
-
+	$(".btn").append("<span>");
+	$(".btn").on("mouseenter", function (e) {
+		var i = $(this),
+				n = i.offset(),
+				s = e.pageX - n.left,
+				a = e.pageY - n.top;
+		i.find("span").css({ top: a, left: s });
+	}).on("mouseout", function (e) {
+		var i = $(this),
+				n = i.offset(),
+				s = e.pageX - n.left,
+				a = e.pageY - n.top;
+		i.find("span").css({ top: a, left: s });
+	});
 
 
 	AOS.init({
-		disable: "mobile",
 		offset: 120,
 		once: true,
 		duration: 1000,
 		easing: "ease"
 	})
+
+	$('.header__burger').on('click',function (){
+		event.preventDefault();
+		$('.header__menu').addClass('active');
+	})
+	$('.hm__close').on('click',function (){
+		event.preventDefault();
+		$('.header__menu').removeClass('active');
+	})
+
+	let delay = 100;
+	$('.hm__item').each(function (){
+		delay += 50;
+		$(this).css('transition-delay',delay/1000+'s');
+	});
+
+	document.querySelectorAll('img')
+			.forEach((img) =>
+					img.addEventListener('load', () =>
+							AOS.refresh()
+					)
+			);
 })();
+
+
+$(window).on('load',function (){
+	let currentTimer = new Date();
+
+	let diff = currentTimer - timerStart;
+	let diffSec = Math.round(diff / 100);
+	if (diffSec < 13){
+		setTimeout(function (){
+			$('#preloader').fadeOut(700,function (){
+				$('#preloader').remove();
+			});
+		},1300-diffSec*100);
+	} else {
+		$('#preloader').fadeOut(700,function (){
+			$('#preloader').remove();
+		})
+	}
+	AOS.refresh();
+});
